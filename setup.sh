d8b445e4aaa59b2beda349745588dd67d2176c09
neverc <root@neverc.cn>
2018-04-25 15:14:18 +0800

    fix sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,16 @@
 echo 'start'
+
+# 变量
+GITHASH=`git rev-parse --short HEAD`
+IMGNAME=netcore-demo
+CONTAINER=netcore-demo-web
+# 发布
 rm -rf ./publish
 dotnet publish -o ./publish
-docker build -t netcore-demo .
+# image
+docker build -t $IMGNAME:$GITHASH .
+docker tag $IMGNAME:$GITHASH $IMGNAME:latest
+docker rmi -f $(docker images -q -f dangling=true)
+# container
+docker stop $CONTAINER || true && docker rm -f $CONTAINER || true
+docker run --name $CONTAINER $IMGNAME
