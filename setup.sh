adadaa8f9b34f4decb4c3ba1255aaa404ae45bb7
neverc <root@neverc.cn>
2018-04-25 15:25:08 +0800

    add -d in run

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 echo 'start'
 
-# 变量
+# variables
 GITHASH=`git rev-parse --short HEAD`
 IMGNAME=netcore-demo
 CONTAINER=netcore-demo-web
-# 发布
+# publish
 rm -rf ./publish
 dotnet publish -o ./publish
 # image
@@ -13,4 +13,4 @@ docker tag $IMGNAME:$GITHASH $IMGNAME:latest
 docker rmi -f $(docker images -q -f dangling=true)
 # container
 docker stop $CONTAINER || true && docker rm -f $CONTAINER || true
-docker run --name $CONTAINER $IMGNAME
+docker run -d --name $CONTAINER $IMGNAME
